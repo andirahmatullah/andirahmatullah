74975dad8463fd7e78e5dc65f9b9d4212e7db0c9
andirahmatullah <99374952+andirahmatullah@users.noreply.github.com>
2022-02-10 09:04:27 +0700

    Add files via upload

---
+++ rahmat pbo 1.py
@@ -0,0 +1,13 @@
+### rumus bangun kerucut
+T = 12
+R = 4
+S = 6
+phi = 22/7
+
+#mengitung luas permukaan dan volume
+
+luas = ( phi * R * R ) + ( phi * R * S )
+print(luas)
+
+volume = 1/3 * phi * R * R * T
+print(volume)
